e0b990e0eaae3a840cee3cfda57eb1fefad9a8f5
chinhtran0911529709 <tranduychinh15012001@gmail.com>
2024-02-01 16:53:06 +0700

    Add project files.

---
+++ Movie_MVC/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movie_MVC.DAO;
+using Movie_MVC.Models;
+using Movie_MVC.Common;
+namespace Movie_MVC.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        // login
+        // Get: Admin/Login
+        
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+  //      [ValidateAntiForgeryToken]
+        public ActionResult Login(string username , string password)
+        {
+            var dao = new UserDAO();
+            var result = dao.Login(username, Encryptor.MD5Hash(password), true);
+            if(result == 1)
+            {
+                // == 1 tuc la admin va dung tk , mk
+                User user = dao.GetByUserName(username);
+                Session.Add("UserName", user.UserName);
+                Session.Add("Password", user.Password);
+                Session.Add("Id", user.UserID);
+                return RedirectToAction("index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ Movie_MVC/DAO/UserDAO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Movie_MVC.Models;
+namespace Movie_MVC.DAO
+{
+    
+    public class UserDAO
+    {
+        MovieProjectV2Entities db = new MovieProjectV2Entities();
+
+        public int Login(string username, string password, bool isLoginAdmin = false)
+        {
+            // search account login 
+            var account_login = db.Users.Where(s => s.UserName == username).FirstOrDefault();
+            // 
+            if(account_login == null)
+            {
+                return 0;
+                // return 0 tuc la khong co tai khoan nao co ten nhu the
+            }
+            else
+            {
+                if(isLoginAdmin == true)
+                {
+                    // == true tuc la dang nhap bang admin
+                    if(account_login.GroupID == "ADMIN" || account_login.GroupID == "MOD")
+                    {
+                        if(account_login.Status == false)
+                        {
+                            // == false tuc la tai khoan k duoc phep hoat dong
+                            return -1;
+                        }
+                        else
+                        {
+                            if(account_login.Password == password)
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return -2;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return -4;
+                    }
+                }
+                else
+                {
+                    return -3;
+                }
+            }
+        }
+        public User GetByUserName(string username)
+        {
+            return db.Users.Where(s => s.UserName == username).FirstOrDefault();
+            
+        }
+    }
+}
